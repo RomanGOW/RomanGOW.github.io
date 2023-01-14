f81ab5dbe3df3bdc978eba03b382d49e80006de3
RomanGOW <56630745+RomanGOW@users.noreply.github.com>
2023-01-14 08:47:58 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const apiUrl = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+const apiKey = 'YOUR_API_KEY';
+
+async function sendMessage() {
+  const message = document.getElementById('message-input').value;
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`
+    },
+    body: JSON.stringify({
+      prompt: message,
+      temperature: 0.5
+    })
+  });
+  const json = await response.json();
+  const responseText = json.choices[0].text;
+  document.getElementById('response').innerText = responseText;
+}
